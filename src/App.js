205e31f0fdccd8bd2eb072b92e55275ed5b87573
nastmi <natan.semrl@gmail.com>
2021-03-10 19:11:28 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import { Input , Button, Flex, Container, Box } from "@chakra-ui/react"
 
 
 export default function App(props, state){
-    const [name, setName] = useState(props);
+    const [name, setName] = useState(props.name);
+    const [input, setInput] = useState();
+
+    function inputChanged(event){
+        const {target: {value}} = event;
+        setInput(value)
+    }
+
+    function changeName(event){
+        setName(input)
+    }
 
     return (
-        <p className={"tst"}>
-            {props.name}
-        </p>
+        <Container>
+            <Box w="100%" p={4}>
+                {name}
+            </Box>
+            <Flex>
+                <Input onChange={inputChanged} placeholder="basic usage"/>
+                <Button onClick={changeName} colorScheme="blue">Button</Button>
+            </Flex>
+        </Container>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from "@chakra-ui/react"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={"sda"}/>
-    <App name={"a"}></App>
-    <test />
+  <ChakraProvider>
+      <App name={"Janez"}/>
+  </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
